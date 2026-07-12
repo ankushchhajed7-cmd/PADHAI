46b5a7f19e817b41be0b7bd516b9e18202d3f229
ankushchhajed7-cmd <ankushchhajed7@gmail.com>
2026-07-12 14:22:43 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'padhai-v1';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./index.html']))
+  );
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
