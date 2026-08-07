734728b7e34c7a313f2440d65e37cfa4a301d105
ankushchhajed7-cmd <ankushchhajed7@gmail.com>
2026-08-07 07:46:53 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,31 @@
+/* PadhAI service worker - network first, self-updating
+   Har update par CACHE ka number badal do: padhai-v3, padhai-v4 ... */
 const CACHE = 'padhai-v2';
+
+// Naya SW turant install ho, purane ka intezaar na kare
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(['./index.html']))
-  );
 });
+
+// Activate hote hi SAARI purani caches mita do
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(names =>
-      Promise.all(names.filter(n => n !== CACHE).map(n => caches.delete(n)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(names => Promise.all(names.map(n => caches.delete(n))))
+      .then(() => self.clients.claim())
   );
 });
+
+// Network pehle. Net chale to hamesha taaza file milegi.
+// Net na ho to cache se chalega (offline support).
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+
   e.respondWith(
     fetch(e.request)
       .then(res => {
-        const resClone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, resClone));
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
         return res;
       })
       .catch(() => caches.match(e.request))
