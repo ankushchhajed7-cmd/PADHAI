963ce49d1e3daedb557274d311b373a94e966a21
ankushchhajed7-cmd <ankushchhajed7@gmail.com>
2026-09-18 08:01:05 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* PadhAI service worker - network first, self-updating
-   Har update par CACHE ka number badal do: padhai-v23, padhai-v23 ... */
-const CACHE = 'padhai-v23';
+   Har update par CACHE ka number badal do: padhai-v24, padhai-v24 ... */
+const CACHE = 'padhai-v24';
 
 // Naya SW turant install ho, purane ka intezaar na kare
 self.addEventListener('install', e => {
